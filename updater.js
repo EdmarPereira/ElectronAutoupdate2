869d774762479681c02917457ebe7b8afaf295a8
Edmar Pereira <edmar.pereira80@gmail.com>
2020-03-18 11:41:10 -0300

    Updater part 2

--- updater.js
+++ updater.js
@@ -1,9 +1,35 @@
-const { autoUpdater } = require('electron-updater')
+const { autoUpdater } = require("electron-updater");
+const { dialog } = require("electron");
 
-autoUpdater.logger = require("electron-log")
-autoUpdater.logger.transports.file.level = "info"
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
 
 module.exports = () => {
-    // console.log('cheking for updates')
-    autoUpdater.checkForUpdates()
-}
+  // console.log('cheking for updates')
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.on("update-available", () => {
+    dialog.showMessageBox(
+      {
+        type: "info",
+        title: "Update available",
+        message: "A new version is available. Do you want to update now?",
+        buttons: ["Update", "No"]
+      },
+      buttonIndex => {
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      }
+    );
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+      dialog.showMessageBox({
+          type: 'info',
+          title: 'Update ready',
+          message: 'Install ans restart now?',
+          buttons: ['Yes', 'Later']
+      }, buttonIndex => {
+          if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true)
+      })
+  })
+};
